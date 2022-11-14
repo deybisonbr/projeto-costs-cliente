dfdb134309d9ad17799cc89554e665b08add543a
Daybison <deybisonbraga@gmail.com>
2022-11-13 23:52:15 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import Projects from './components/pages/Projects';
+import NewProject from './components/pages/NewProject';
+import Project from './components/pages/Project';
+import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/newproject" element={<NewProject />} />
+          <Route path="/project/:id" element={<Project />} />
+        </Routes>
+      </Container>
+
+      <Footer/>
+    </Router>
+  );
+}
+
+export default App;
